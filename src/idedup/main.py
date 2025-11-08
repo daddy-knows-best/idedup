dbf534273c1ddc2601623af66d742ec68ba2439f
SeungYeop Yang <seungyeop.yang@gmail.com>
2025-11-08 11:44:56 -0600

    updated

--- src/idedup/main.py
+++ src/idedup/main.py
@@ -3,13 +3,19 @@ import sys
 
 
 def main():
+    idedup_alt(True)
+
+
+def idedup_alt(first):
     indexed_dedup = dict()
     last_line = 0
     for index, line in enumerate(sys.stdin, start=1):
         line_strip = line.strip()
         last_line = index
-        if line_strip in indexed_dedup:
-            continue
+        if first:
+            if line_strip in indexed_dedup:
+                continue
+
         indexed_dedup[line_strip] = index
 
     number_of_digit = (last_line // 10) + 5
